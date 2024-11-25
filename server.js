5b0d0dfee05a7c11dc799dfd61a3bf033ac51df9
aDJi2003 <adjidharmawan87@gmail.com>
2024-11-25 20:50:12 +0700

    test

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ app.use((req, res, next) => {
     next();
 });
 
+require('dotenv').config();
+
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
 
